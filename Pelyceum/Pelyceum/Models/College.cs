9dede1f4e912740f5e54a3a521536be401dc4ecf
Brian Golden <bgold09@users.noreply.github.com>
2019-02-03 12:53:55 -0800

    Move to UWP (#3)
      * Universal Windows Platform (UWP) lets you build apps for any Windows device.
      * See https://aka.ms/uwp for more information.
      * Enable CI with Azure Pipelines.

---
+++ Pelyceum/Pelyceum/Models/College.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelyceum
+{
+    class College
+    {
+        public string name;
+        public string loc;
+        public string mail;
+        public string phone;
+        public string site;
+        //list? majors;
+        public int avtuit;
+        //list? scholarships
+        //list? deadlines
+        public Boolean comapp;
+
+
+        public College(string entname, int entuit)
+        {
+            name = entname;
+            avtuit = entuit;
+        }
+
+
+
+        IDictionary<string, College> collegeList = new Dictionary<string, College>();
+        //majors, scholarships, emails, phones, etc.
+        //list of majors - arraylist?
+        //create a search function
+    }
+}
