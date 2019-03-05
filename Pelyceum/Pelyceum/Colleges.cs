731b54239908bf32d72fb89174def44a4db696f2
gwhelan19 <gwhelan19@holynames-sea.org>
2019-03-05 07:37:24 -0800

    Grace/student (#5)
    * working on the methods in the student class
    
    * worked on the main class of colleges with the search method and worked on the main page of the student class

---
+++ Pelyceum/Pelyceum/Colleges.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelyceum
+{
+    class Colleges
+    {
+       static void Main(string[] args)
+        {
+            System.Collections.ArrayList CollegeList = new System.Collections.ArrayList();
+            CollegeList.Add("University of Wahsington");
+            CollegeList.Add("Duke Univeristy");
+            CollegeList.Add("Harvard College");
+            CollegeList.Add("Stanford Univeristy");
+            CollegeList.Add("Cornell University");
+            CollegeList.Add("Santa Clara University");
+            CollegeList.Add("Brown University");
+            CollegeList.Add("Columbia University");
+            CollegeList.Add("Dartmouth University");
+            CollegeList.Add("Purdue University");
+            CollegeList.Add("Tufts University");
+            CollegeList.Add("Boston College");
+            CollegeList.Add("Yale University");
+            CollegeList.Add("University of Pennsylvania");
+            CollegeList.Add("Princeton University");
+            CollegeList.Add("Georgetown University");
+            CollegeList.Add("Wellesley College");
+            CollegeList.Add("University of Southern California");
+            CollegeList.Add("California State Polytechnic Institute");
+
+        }
+
+
+
+
+    }
+}
